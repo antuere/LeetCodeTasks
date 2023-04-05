652936d0fa2924601e5535b7c6cb62259ca14ca1
antuere <antuere@yandex.ru>
2023-04-05 15:11:06 +0300

    Add new tasks 695,733,2439

---
+++ src/main/kotlin/algorithms/695_MaxAreaOfIsland.kt
@@ -0,0 +1,72 @@
+package algorithms
+
+/*
+You are given an m x n binary matrix grid.
+An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.)
+You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid.
+If there is no island, return 0.
+
+Input: grid =[
+[0,0,1,0,0,0,0,1,0,0,0,0,0],
+[0,0,0,0,0,0,0,1,1,1,0,0,0],
+[0,1,1,0,1,0,0,0,0,0,0,0,0],
+[0,1,0,0,1,1,0,0,1,0,1,0,0],
+[0,1,0,0,1,1,0,0,1,1,1,0,0],
+[0,0,0,0,0,0,0,0,0,0,1,0,0],
+[0,0,0,0,0,0,0,1,1,1,0,0,0],
+[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+*/
+
+fun main() {
+    val sea = arrayOf(
+        intArrayOf(0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+        intArrayOf(0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0),
+        intArrayOf(0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+        intArrayOf(0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0),
+        intArrayOf(0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0),
+        intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0),
+        intArrayOf(0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0),
+        intArrayOf(0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0),
+    )
+
+    maxAreaOfIsland(grid = sea)
+}
+
+fun maxAreaOfIsland(grid: Array<IntArray>): Int {
+    var result = 0
+
+    val m = grid.size
+    val n = grid[0].size
+
+    for (i in 0 until m) {
+        for (j in 0 until n) {
+            if (grid[i][j] == 1) {
+                val currentResult = dfs(grid, i, j)
+                result = Math.max(currentResult, result)
+            }
+        }
+    }
+
+    println("Result is $result")
+    return result
+}
+
+fun dfs(grid: Array<IntArray>, r: Int, s: Int): Int {
+    var islandSize = 0
+    if (grid[r][s] == 1) {
+        islandSize++
+        grid[r][s] = 0
+
+        if (r >= 1) islandSize += dfs(grid, r - 1, s)
+        if (r + 1 < grid.size) islandSize += dfs(grid, r + 1, s)
+        if (s >= 1) islandSize += dfs(grid, r, s - 1)
+        if (s + 1 < grid[0].size) islandSize += dfs(grid, r, s + 1)
+    }
+    return islandSize
+}
---
+++ src/main/kotlin/algorithms/733_FloodFill.kt
@@ -0,0 +1,55 @@
+package algorithms
+
+/*
+An image is represented by an m x n integer
+grid image where image[i][j] represents the pixel value of the image.
+
+You are also given three integers sr, sc, and color.
+You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill, consider the starting pixel,
+plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel,
+plus any pixels connected 4-directionally to those pixels (also with the same color), and so on.
+Replace the color of all of the aforementioned pixels with color.
+
+Return the modified image after performing the flood fill.
+
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) ,
+all pixels connected by a path of the same color as the starting pixel are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+*/
+
+fun main() {
+    floodFill(
+        image = arrayOf(intArrayOf(1, 1, 1), intArrayOf(1, 1, 0), intArrayOf(1, 0, 1)),
+        sr = 1,
+        sc = 1,
+        color = 2
+    )
+}
+
+fun floodFill(image: Array<IntArray>, sr: Int, sc: Int, color: Int): Array<IntArray> {
+    val startColor = image[sr][sc]
+    if (startColor != color) {
+        dfs(image, sr, sc, color, startColor)
+    }
+
+    image.forEach {
+        println(it.toList())
+    }
+    return image
+
+}
+
+fun dfs(image: Array<IntArray>, r: Int, c: Int, color: Int, startColor: Int) {
+    if (image[r][c] == startColor) {
+        image[r][c] = color
+
+        if (r - 1 >= 0) dfs(image, r - 1, c, color, startColor)
+        if (r + 1 < image.size) dfs(image, r + 1, c, color, startColor)
+        if (c - 1 >= 0) dfs(image, r, c - 1, color, startColor)
+        if (c + 1 < image[0].size) dfs(image, r, c + 1, color, startColor)
+    }
+}
